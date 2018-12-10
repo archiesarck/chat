fd594f453b6bdbd46231738a8268cc17cdd94f92
Archies Arck <37809902+arkches@users.noreply.github.com>
2018-12-10 15:35:47 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.sendfile('chat.html');
+	res.sendFile(__dirname+'/chat.html');
 });
 
 users = [];
