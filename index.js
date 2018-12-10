91cca1c1c20c502b5c1a84d5e8667314e7f5e79e
Archies Arck <37809902+arkches@users.noreply.github.com>
2018-12-10 14:30:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendfile('chat.html');
+});
+
+users = [];
+io.sockets.on('connection', function(socket){
+	console.log('A user connected!');
+	socket.on('setUsername', function(data){
+		console.log(data);
+		if(users.indexOf(data) > -1){
+			socket.emit('userExists', data + ' username is taken!');
+		}
+		else{
+			users.push(data);
+			socket.join(data);
+			socket.emit('userSet', {user: data});
+			io.sockets.emit('online', users);
+		}
+	});
+
+	socket.on('msg', function(data){
+		//console.log(data);
+		var to = data.to;
+		var message = data.message;
+		var user = data.from;
+		io.sockets.in(to).emit('newmsg', {message: message, from: user});
+	});
+});
+
+http.listen(80);
